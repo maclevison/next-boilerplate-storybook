421ee8dffdd0ad8f66bbf0d1b6c16b0143eb179f
Maclevison Silva <maclevison@gmail.com>
2020-08-05 12:08:04 +0100

    add .prettierignore

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
+// eslint-disable-next-line no-undef
 const isProd = process.env.NODE_ENV === 'production'
 
+// eslint-disable-next-line no-undef
 module.exports = withPWA({
   pwa: {
     dest: 'public',
